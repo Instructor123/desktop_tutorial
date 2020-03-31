b713032c909b90463e782f952ce3c9ffd38c472e
Instructor123 <alex.wollman@dsu.edu>
2020-03-30 22:29:49 -0500

    Initial commit
    This is my initial commit with master.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    int i = 0;
+    int var1 = 10;
+    int var2 = 100;
+
+    if( var1 > 5 ){
+        cout<<"var1 greater than 5"<<endl;
+    } else {
+        cout<<"var1 is not greater than 5"<<endl;
+    }
+
+    return 0;
+}
