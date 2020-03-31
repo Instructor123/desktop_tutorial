b1e6e8db641ae3886cc14d7851fcb80d41b908bf
Instructor123 <alex.wollman@dsu.edu>
2020-03-30 22:30:30 -0500

    Formatting code
    Added comments and some styling to format my code.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
+/*
+    Main function
+*/
 int main(int argc, char *argv[]){
 
+    //variables
     int i = 0;
     int var1 = 10;
     int var2 = 100;
 
+    //code begins
     if( var1 > 5 ){
         cout<<"var1 greater than 5"<<endl;
     } else {
